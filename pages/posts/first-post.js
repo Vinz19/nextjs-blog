c66ffe7baae9493d07b1e3fe71fa3a8bc7f90e61
Vinz19 <vinzparis@mail.fr>
2022-03-11 17:37:56 +0100

    Add an example of a script, but not important for the exercise

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,28 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import Script from "next/script";
 
 export default function FirstPost() {
-   
-    return (
-        <>
+  return (
+    <>
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
-        <Image
-          src="/images/profile.jpg"
-          height={150}
-          width={150}
-          alt="Vinz Starfoullah"
-        />
+      <Image
+        src="/images/profile.jpg"
+        height={150}
+        width={150}
+        alt="Vinz Starfoullah"
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
