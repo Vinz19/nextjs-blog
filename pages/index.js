b888650187136c0a9218ecf704d986345ab5d0d1
Vinz19 <vinzparis@mail.fr>
2022-03-14 15:21:03 +0100

    Import and use Link component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+
+import Link from 'next/link'
+import Date from '../components/date'
+
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
@@ -31,12 +35,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
